9d4964d08873175dff042a6c3456b68320e7da00
kmrajibfaysal <kmrajibfaysal@gmail.com>
2021-07-28 00:23:13 +0600

    adding file

---
+++ main.js
@@ -0,0 +1,32 @@
+class Store {
+  constructor(name) {
+    this.name = name;
+    this.items = [];
+    this.prices = {};
+  }
+  isItemAvailable(name) {
+    let itemIndex = this.items.indexOf(name);
+    if (itemIndex == -1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  getPrice(name) {
+    let isAvailable = this.isItemAvailable(name);
+    if (isAvailable == true) {
+      let itemPrice = this.prices[name];
+      return itemPrice;
+    } else {
+      console.log("Sorry. We do not have the item.");
+    }
+  }
+  addItem(name, price) {
+    this.items.push(name);
+    this.prices[name] = price;
+  }
+}
+
+let akijStore = new Store("Akij Fashion Ltd.");
+akijStore.addItem("shirt", 300);
+akijStore.addItem("pant", 500);
